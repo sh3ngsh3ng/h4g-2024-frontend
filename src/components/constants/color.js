6b38cebb53214bad34e59d537b5ca6edf2361bff
chiageng <chiageng12@gmail.com>
2024-02-02 23:16:48 +0800

    SignUpForm and create design system

---
+++ src/components/constants/color.js
@@ -0,0 +1,11 @@
+export const primaryRed = "#D1000A";
+export const secondaryRed = "#F2668F";
+export const tertiaryRed = "#E07075";
+export const paleRed = "#FEDDDC";
+export const palestRed = "#FEF1F5";
+export const white = "#FFFFFF";
+
+
+export const redButtonActive = "#BA333A"
+export const redButtonHover = "#FEDDDC"
+export const redButtonFocus = "#D1000A"
