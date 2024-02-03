068d100d2381a42f253929adc803856d9a1d7612
zhanyang01 <ngzhanyang01@gmail.com>
2024-02-03 11:18:25 +0800

    remove lag when clicking button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,21 @@
 import './Navbar.css'
+
 export default function Navbar() {
     return (
-        <nav><span>Navbar</span></nav>
+        <div className="navbar">
+            <div className="navbar__left">
+                <img
+                    src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1619464538~hmac=5e4f4e6a9b0d3c9b3d3f7f9e4b2f0e5d"
+                    alt="logo"
+                />
+                <h2>Opportunities</h2>
+            </div>
+            <div className="navbar__right">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <a href="#login">Login</a>
+            </div>
+        </div>
     )
 }
