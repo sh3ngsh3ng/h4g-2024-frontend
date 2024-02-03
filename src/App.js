f8a9c1aa2a90fac1f2e3c0e90b6d2bf10780509e
ys <ysdeveloper01@gmail.com>
2024-02-03 16:30:57 +0800

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 // Pages
 import LandingPage from "../src/pages/landing-page/LandingPage";
 import ErrorPage from "../src/pages/error-page/ErrorPage";
-import LoginPage from "./pages/auth-page/LoginPage";
 import AuthPage from "./pages/auth-page/AuthPage";
 
 // Components
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import RegisterPage from "./pages/auth-page/RegisterPage";
 
 function App() {
   return (
@@ -25,8 +22,6 @@ function App() {
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
           <Route path="/signup" element={<AuthPage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
         </Routes>
       </main>
     </ChakraProvider>
--- src/pages/auth-page/AuthPage.js
+++ src/pages/auth-page/AuthPage.js
@@ -3,7 +3,6 @@ import SignUpForm from "../../components/forms/SignUpForm";
 import AuthTab from "../../components/utilities/AuthTab";
 import Carousel from "../../components/utilities/Carousel";
 import { Box, Flex, Show, Hide } from "@chakra-ui/react";
-import { useState } from "react";
 
 export default function AuthPage() {
   return (
@@ -15,7 +14,6 @@ export default function AuthPage() {
             <Carousel />
           </Box>
         </Show>
-
         <Box w={{ base: "100%", lg: "50%" }} padding={5}>
           <AuthTab tab1={<LoginForm />} tab2={<SignUpForm />} />
         </Box>
