8e11fcae91ff4ed9a2c526e799c92f3748f992d8
ys <ysdeveloper01@gmail.com>
2024-02-07 03:10:42 +0800

    added tabs for user

--- src/pages/user-page/UserPage.jsx
+++ src/pages/user-page/UserPage.jsx
@@ -1,6 +1,6 @@
 import { auth } from "../../firebase/firebase"
 import Navbar from "../../components/navbar/Navbar"
-import { Box, Stack, Button, Icon } from "@chakra-ui/react"
+import { Box, Stack, Button, Tabs, TabList, Tab } from "@chakra-ui/react"
 import { ArrowDownIcon } from "@chakra-ui/icons"
 import EventCard from "../../components/utilities/EventCard"
 
@@ -14,6 +14,12 @@ export default function UserPage() {
         <>
             <Box h="100%" w="100%" p={3}>
                 <Navbar />
+                <Tabs variant='enclosed'>
+                    <TabList>
+                        <Tab>Current</Tab>
+                        <Tab>Past</Tab>
+                    </TabList>
+                </Tabs>
                 <Stack direction="row" spacing={4}>
                     <Button rightIcon={<ArrowDownIcon />} bgColor="pink">
                         Sort
