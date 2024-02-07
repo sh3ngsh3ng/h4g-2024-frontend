0a56771814f636a2a60afdba9134b5226436b9ab
ys <ysdeveloper01@gmail.com>
2024-02-08 01:29:20 +0800

    added store for events, added loading of data from backend on adminpage loaded (to change to after login)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userFirebaseRegisterReducers, userLoginReducers, userRegisterReducers } from "../components/reducers/userReducer";
 import { adminDashboardReducers, adminEventsReducers } from "../components/reducers/adminReducer";
+import { eventsReducers } from "../components/reducers/eventsReducer";
 
 export const store = configureStore({
     reducer: {
@@ -8,6 +9,7 @@ export const store = configureStore({
         userRegister: userRegisterReducers,
         userFirebaseRegister: userFirebaseRegisterReducers,
         adminDashboard: adminDashboardReducers,
-        adminEvents: adminEventsReducers
+        adminEvents: adminEventsReducers,
+        events: eventsReducers
     }
 })
