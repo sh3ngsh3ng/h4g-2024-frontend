4c37e38f48c028f30e04ea229b6e1396ad25bb99
ys <ysdeveloper01@gmail.com>
2024-02-08 15:27:58 +0800

    added double binding for interest and skills field

--- src/components/constants/admin.js
+++ src/components/constants/admin.js
@@ -1,11 +1,31 @@
 export const ADMIN_DASHBOARD_MODE_CREATE = "ADMIN_DASHBOARD_MODE_CREATE"
 export const ADMIN_DASHBOARD_MODE_READ = "ADMIN_DASHBOARD_MODE_READ"
 export const ADMIN_DASHBOARD_MODE_UPDATE = "ADMIN_DASHBOARD_MODE_UPDATE"
+// events
+// export const EVENT_FORM_TEMPLATE = {
+//     name: "",
+//     organization: "",
+//     skills: [],
+//     month: "",
+//     day: "",
+//     description: ""
+// }
 export const EVENT_FORM_TEMPLATE = {
-    name: "",
-    organization: "",
-    skills: [],
-    month: "",
-    day: "",
-    description: ""
+    "volunteers": [],
+    "_id": "65be2f3742113c61cfd10ace",
+    "name": "1800",
+    "slug": "1800",
+    "startDate": "2024-12-10T00:00:00.000Z",
+    "endDate": "2024-12-17T00:00:00.000Z",
+    "description": "     ",
+    "maxHoursGiven": 28,
+    "interest": ["Category 1"],
+    "skills": [],
+    "isCompleted": false,
+    "createdAt": "2024-02-03T12:19:03.627Z",
+    "updatedAt": "2024-02-03T14:48:01.632Z",
+    "__v": 0
 }
+// list of 
+export const INTERESTS_LIST = ["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"]
+export const SKILLS_LIST = ["Skill 1", "Skill 2", "Skill 3", "Skill 4", "Skill 5"]
